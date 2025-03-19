41faf158cab14aeee3a4c9cc791aece91b323b67
Lisa Collins <Lisa.Collins@veteransunited.com>
2025-03-19 09:41:43 -0500

    LisaCollinsClassyPerson

--- classy_person.py
+++ classy_person.py
@@ -9,6 +9,20 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+
+    def increase_age(self):
+        self.age = self.age +1
+
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+
+    def count_to_age(self):
+        for number in range(1, self.age+1):
+            print(number)
 
 
 
